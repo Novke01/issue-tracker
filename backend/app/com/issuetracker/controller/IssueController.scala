b6e4c038cd76bc5c19f46ef903fecb0e0ca3dfdf
Aleksandar Kahriman <akahriman@execom.eu>
2018-02-10 22:36:53 +0100

    Minor fixes.

--- backend/app/com/issuetracker/controller/IssueController.scala
+++ backend/app/com/issuetracker/controller/IssueController.scala
@@ -4,7 +4,7 @@ import play.api.mvc._
 import play.api.libs.json._
 
 import scala.concurrent.{ExecutionContext, Future}
-import com.issuetracker.dto.{PostIssue, UpdateIssue}
+import com.issuetracker.dto.{JwtUser, PostIssue, UpdateIssue}
 import com.issuetracker.service.{AssignedUserService, IssueLabelService, IssueService}
 import com.issuetracker.util.JwtUtil
 import play.api.Logger
@@ -16,17 +16,19 @@ class IssueController(val cc: ControllerComponents,
                       val jwtUtil: JwtUtil)(implicit val executionContext: ExecutionContext)
     extends AbstractController(cc) {
 
-  val logger: Logger = Logger(this.getClass())
+  val logger: Logger = Logger(this.getClass)
 
   def insert: Action[JsValue] = Action.async(parse.json) { request =>
+    val currentUser = request.attrs.get(JwtUser.Key).getOrElse {
+      NotFound
+    }.asInstanceOf[JwtUser]
     val optionalIssue = request.body.validate[PostIssue]
     Future {
       BadRequest
     }
     optionalIssue match {
       case JsSuccess(postIssue: PostIssue, _) =>
-        val ownerId: Long     = jwtUtil.decode(request) map { _.id } getOrElse { -1 }
-        val modifiedPostIssue = postIssue.copy(ownerId = ownerId)
+        val modifiedPostIssue = postIssue.copy(ownerId = currentUser.id)
         issueService.insert(modifiedPostIssue).map { result =>
           Created(Json.toJson(result))
         } recover {
@@ -71,37 +73,37 @@ class IssueController(val cc: ControllerComponents,
     }
   }
 
-  def insertAssignee(issueId: Long, userId: Long) = Action.async {
+  def insertAssignee(issueId: Long, userId: Long): Action[AnyContent] = Action.async {
     assignedUserService.insertAssignee(issueId, userId) map { result =>
-      Ok("")
+      Ok
     }
   }
 
-  def removeAssignee(issueId: Long, userId: Long) = Action.async {
+  def removeAssignee(issueId: Long, userId: Long): Action[AnyContent] = Action.async {
     assignedUserService.removeAssignee(issueId, userId) map { result =>
       Ok(Json.toJson(result))
     }
   }
 
-  def getAssignees(issueId: Long) = Action.async {
+  def getAssignees(issueId: Long): Action[AnyContent] = Action.async {
     assignedUserService.findAssigneesByIssueId(issueId) map { result =>
       Ok(Json.toJson(result))
     }
   }
 
-  def getLabels(issueId: Long) = Action.async {
+  def getLabels(issueId: Long): Action[AnyContent] = Action.async {
     issueLabelService.findLabelsByIssueId(issueId) map { result =>
       Ok(Json.toJson(result))
     }
   }
 
-  def insertLabel(issueId: Long, labelId: Long) = Action.async {
-    issueLabelService.insertLabel(issueId, labelId) map { result =>
-      Ok("")
+  def insertLabel(issueId: Long, labelId: Long): Action[AnyContent] = Action.async {
+    issueLabelService.insertLabel(issueId, labelId) map { _ =>
+      Ok
     }
   }
 
-  def removeLabel(issueId: Long, labelId: Long) = Action.async {
+  def removeLabel(issueId: Long, labelId: Long): Action[AnyContent] = Action.async {
     issueLabelService.removeLabel(issueId, labelId) map { result =>
       Ok(Json.toJson(result))
     }
